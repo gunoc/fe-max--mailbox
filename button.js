4a5a79a137b9abdddf3fcb4f522304a1a2b6b1f1
wonkyung <mwk0725@gmail.com>
2023-03-31 15:44:01 +0900

    Feat: 버튼 클릭시 우체통 가진 마을정보 표시

--- button.js
+++ button.js
@@ -7,7 +7,39 @@ checkMailboxBtn.addEventListener('click', handleClickbutton);
 function handleClickbutton() {
   const mailBoxes = customQuerySelectorAll(root, '.mail-box');
 
+  showInfoTextBox(mailBoxes);
   changeStyleVillagesWithMailbox(mailBoxes);
+}
+
+function showInfoTextBox(mailBoxes) {
+  const [villageInfo, mailInfo] = customQuerySelectorAll(root, '.info-text');
+  villageInfo.classList.remove('hide');
+  mailInfo.classList.remove('hide');
+  mailboxInfo(mailBoxes);
+}
+
+function mailboxInfo(mailBoxes) {
+  const mailboxsData = setVillagesWithMailbox(mailBoxes);
+  totalVillagesWithMailbox(mailboxsData);
+}
+
+function setVillagesWithMailbox(mailBoxes) {
+  const mailboxsWithIdAndSize = [];
+
+  mailBoxes.forEach(mailBox => {
+    const size = parseInt(mailBox.dataset.size);
+    const id = mailBox.parentElement.id;
+    mailboxsWithIdAndSize.push({ id, size });
+  });
+
+  return mailboxsWithIdAndSize;
+}
+
+function totalVillagesWithMailbox(mailBoxes) {
+  const villageInfoEl = customQuerySelector(root, '.village-info-text');
+  const villageCount = mailBoxes.length;
+  const villageNames = mailBoxes.map(box => box.id).join(', ');
+  villageInfoEl.textContent = `${villageNames} 총 ${villageCount}개의 마을 입니다.`;
 }
 
 function changeStyleVillagesWithMailbox(mailBoxes) {
